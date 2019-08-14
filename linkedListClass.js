6a0d765a6a1183e600620e4b7450c02f241849de
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-08-14 16:43:22 -0400

    added pop method

--- linkedListClass.js
+++ linkedListClass.js
@@ -24,10 +24,30 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
 }
 
 let list = new LinkedList();
 list.push(5);
 list.push(50);
 list.push(500);
+list.pop();
 console.log(list);
