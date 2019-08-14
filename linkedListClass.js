5cbc5710751b2251e2af688b6e76759ac28a01ed
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-08-14 11:42:00 -0400

    initial commit

---
+++ linkedListClass.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(value) {
+    this.val = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new LinkedList();
+list.push(5);
+list.push(50);
+list.push(500);
+console.log(list);
