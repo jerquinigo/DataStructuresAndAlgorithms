94ff5b6e6d5b151858f665db0339b4e30efd1b7f
jerquinigo <jonathanerquinigo@pursuit.org>
2020-02-08 22:40:26 -0500

    adding another problem

--- praticeProblems/recursionProblems.js
+++ praticeProblems/recursionProblems.js
@@ -231,3 +231,25 @@ const countEars = (num) => {
 }
 
 countEars(2)
+
+
+// Given a string and a non-empty substring sub, compute recursively the number of times that sub appears in the string, without the sub strings overlapping.
+
+
+// strCount("catcowcat", "cat") → 2
+// strCount("catcowcat", "cow") → 1
+// strCount("catcowcat", "dog") → 0
+
+
+
+
+const strCount = (str, sub) => {
+  if(!str) return 0;
+  let [start, end] = [0, sub.length];
+  if(str.slice(start,end) === sub)return 1 + strCount(str.slice(end),sub)
+  else return strCount(str.slice(++start),sub)
+}
+
+strCount("catcowcat", "cat")
+
+
