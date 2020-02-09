a8f340619aeb93676873c18f9d045c7546fcad96
jerquinigo <jonathanerquinigo@pursuit.org>
2020-02-08 21:38:17 -0500

    adding another problem

--- praticeProblems/recursionProblems.js
+++ praticeProblems/recursionProblems.js
@@ -189,3 +189,21 @@ const changePi = (str) => {
 }
 
 changePi("xpix")
+
+
+// We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
+
+
+// triangle(0) → 0
+// triangle(1) → 1
+// triangle(2) → 3
+
+const triCheck = (num) => {
+  if(num === 0) return num
+  let count = 0
+  if(num !== 0){
+    return num + triCheck(--num)
+  }
+}
+
+triCheck(2)
