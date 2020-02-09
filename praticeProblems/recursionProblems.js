4948d2c3eed70e60ea7dd135fa3d1bc9a92428ff
jerquinigo <jonathanerquinigo@pursuit.org>
2020-02-08 21:00:47 -0500

    added another problem

--- praticeProblems/recursionProblems.js
+++ praticeProblems/recursionProblems.js
@@ -154,3 +154,25 @@ const changeChar = (str) => {
 
 
 changeChar("codex")
+
+
+// Given a string, return true if it is a nesting of zero or more pairs of parenthesis, like "(())" or "((()))". Suggestion: check the first and last chars, and then recur on what's inside them.
+
+
+// nestParen("(())") → true
+// nestParen("((()))") → true
+// nestParen("(((x))") → false
+
+const checkNest = (str) => {
+  if(!str) return true
+  let backIdx = str.length -1 
+  if(str[0] + str[backIdx] === "()"){
+    return checkNest(str.slice(1,-1))
+  }
+  else{
+    return false
+  }
+
+}
+
+checkNest("(())")
