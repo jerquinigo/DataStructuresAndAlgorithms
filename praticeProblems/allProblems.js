731b389fa5888e731b91fd00800e2280694bf997
jerquinigo <jonathanerquinigo@pursuit.org>
2020-02-13 20:27:45 -0500

    adding link

--- praticeProblems/allProblems.js
+++ praticeProblems/allProblems.js
@@ -1,5 +1,7 @@
 // Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
 
+//https://leetcode.com/problems/unique-number-of-occurrences/
+
 
 const uniqueOccurance = (arr) => {
     let dict= {}
